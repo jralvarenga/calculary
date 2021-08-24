263d41de8213534bd73e7a1f69a78a1b0bd69490
jralvarenga <jralvarenga161@gmail.com>
2021-08-23 23:39:28 -0600

    Dinamic color change

--- lib/services/format_color.dart
+++ lib/services/format_color.dart
@@ -16,24 +16,24 @@ class FormatColor {
     if (this.isDark) {
       switch (primaryString) {
         case 'lila':
-          return Color.fromRGBO(198, 112, 255, 1);
+          return Color.fromRGBO(215, 177, 227, 1);
         case 'alphaca':
-          return Color.fromRGBO(86, 178, 84, 1);
+          return Color.fromRGBO(132, 255, 130, 1);
         case 'blossom':
           return Color.fromRGBO(255, 94, 125, 1);
         default:
-          return Color.fromRGBO(198, 112, 255, 1);
+          return Color.fromRGBO(215, 177, 227, 1);
       } 
     } else {
       switch (primaryString) {
         case 'lila':
-          return Color.fromRGBO(188, 102, 255, 1);
+          return Color.fromRGBO(182, 149, 192, 1);
         case 'alphaca':
-          return Color.fromRGBO(76, 168, 74, 1);
+          return Color.fromRGBO(109, 214, 107, 1);
         case 'blossom':
           return Color.fromRGBO(255, 84, 115, 1);
         default:
-          return Color.fromRGBO(188, 102, 255, 1);
+          return Color.fromRGBO(182, 149, 192, 1);
       }
     }
   }
@@ -43,22 +43,22 @@ class FormatColor {
     if (this.isDark) {
       switch (accentString) {
         case 'peach':
-          return Color.fromRGBO(255, 213, 175, 1);
+          return Color.fromRGBO(255, 212, 181, 1);
         case 'mango':
-          return Color.fromRGBO(255, 176, 53, 1);
+          return Color.fromRGBO(255, 224, 112, 1);
         case 'lavanda':
-          return Color.fromRGBO(218, 166, 255, 1);
+          return Color.fromRGBO(212, 163, 247, 1);
         default:
-          return Color.fromRGBO(255, 213, 175, 1);
+          return Color.fromRGBO(255, 212, 181, 1);
       } 
     } else {
       switch (accentString) {
         case 'peach':
           return Color.fromRGBO(255, 203, 165, 1);
         case 'mango':
-          return Color.fromRGBO(255, 166, 43, 1);
+          return Color.fromRGBO(255, 217, 79, 1);
         case 'lavanda':
-          return Color.fromRGBO(181, 126, 220, 1);
+          return Color.fromRGBO(203, 141, 247, 1);
         default:
           return Color.fromRGBO(255, 203, 165, 1);
       }
@@ -71,6 +71,6 @@ Color estimateBrightnessForColorForText(Color color) {
 
   const double kThreshold = 0.15;
   if ((relativeLuminance + 0.05) * (relativeLuminance + 0.05) > kThreshold)
-    return Color.fromRGBO(0, 0, 0, 1);
+    return Color.fromRGBO(50, 50, 50, 1);
   return Color.fromRGBO(255, 255, 255, 1);
 }
