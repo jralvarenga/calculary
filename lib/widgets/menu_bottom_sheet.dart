0472e583bbd46c6fdc4370857c5293ff3dba3a1a
jralvarenga <jralvarenga161@gmail.com>
2021-08-26 22:32:19 -0600

    Added all translations

--- lib/widgets/menu_bottom_sheet.dart
+++ lib/widgets/menu_bottom_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:calculary/services/custom_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
